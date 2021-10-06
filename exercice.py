c7f9dae45f50fdb8d7ea0f728df6f47827046991
sebroy-6 <sebastien-6.roy@polymtl.ca>
2021-10-06 17:14:54 -0400

    Completed Version

--- exercice.py
+++ exercice.py
@@ -3,12 +3,60 @@
 
 # TODO: Importez vos modules ici
 
+import math
+from exercice_ch6 import frequence
+from turtle import *
 
 
 # TODO: Définissez vos fonction ici
+
+def volume_masse_ellipsoide(a=1, b=1, c=1, masse_volumique=1):
+    V = (4 / 3)*math.pi * a * b * c
+    masse = masse_volumique * V
+    return V, masse
+
+
+def draw_tree(pen_size=6, length=8, angle=30):  # Revoir avec le prof pour savoir comment simplifier la construction du code #3
+    # Starting sequence
+    penup()
+    goto(0, -250)
+    pendown()
+    left(90)
+    length *= 10
+    color("BROWN")
+    # Starts drawing
+    draw_branche(pen_size, length, angle)
+    done()
+
+def draw_branche(pen_size, length, angle):  # It goes directly to the deepest node and, when the first (rigth)
+    # and second (left) draw_branche(), it goes backwards once and does the second (left) draw_brach() (does this for every node
+    # in the tree
+    if length > 0 and pen_size > 0:
+        if length == 10 or pen_size == 1:
+            color("GREEN")
+        pensize(pen_size)
+        forward(length)
+        right(angle)
+        draw_branche(pen_size - 1, length - 10, angle - 5)  # Lignes 36 a 40 : Je n'arrive pas à visualiser le processus
+        left(angle * 2)
+        draw_branche(pen_size - 1, length - 10, angle - 5)
+        if length > 20 and pen_size > 2:
+            color("BROWN")
+        right(angle)
+        backward(length)
+
+
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
 
-    pass
+    print("Exercice #1:", volume_masse_ellipsoide(10, 8, 6))  # Exercice 1
+
+
+    most_frequent_letter = lambda texte: list(frequence(texte))[list(frequence(texte).values())
+        .index(max(list(frequence(texte).values())))]
+
+    print("Exercice #2:", most_frequent_letter("hello world"))  # Exercice 2
+
+    draw_tree(6, 10, 30)  # Exercice 3
---
+++ exercice_ch6.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def order(values: list = None) -> list:
+    if values is None:
+        # TODO: demander les valeurs ici
+
+        values = list()
+
+        while len(values) != 10:
+            print("Veuiller s'il vous plait insérer 10 valeurs avec des espaces entre elles: \n")
+            values = input().split(" ")
+
+            if len(values) != 10:
+                print("Vous n'avez pas inscrit les variables correctement, veuillez réessayer")
+                values = list()
+
+    return sorted(values)
+
+
+def anagrams(words: list = None) -> bool:
+    if words is None:
+        # TODO: demander les mots ici
+
+        print("Veuillez inscrire le premier mot : \n")
+        words = [input()]
+
+        print("Veuillez inscrire le deuxième mot : \n")
+        words.append(input())
+
+        for letter in words[0]:
+            return letter in words[1]
+
+    else:
+        for letter in words[0]:
+            return letter in words[1]
+
+def contains_doubles(items: list) -> bool:
+
+    return list(set(items)) != items
+
+
+def best_grades(student_grades: dict) -> dict:
+    # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
+
+    best_average = 0
+    best_student = ""
+
+    for name, grades in student_grades.items():
+        student_grades[name] = sum([note for note in grades]) / (len(grades))
+
+    for student in student_grades:
+        if student_grades[student] > best_average:
+            best_average = student_grades[student]
+            best_student = student
+
+    return {best_student: best_average}
+
+
+def frequence(sentence: str) -> dict:
+    # TODO: Afficher les lettres les plus fréquentes
+    #       Retourner le tableau de lettres
+
+    letter_dictionnary = dict()
+    for letter in sentence:
+        if letter in letter_dictionnary:
+            letter_dictionnary[letter] += 1
+        else:
+            letter_dictionnary[letter] = 1
+
+    return letter_dictionnary
+
+
+def get_recipes():
+    # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
+
+    livre_recette = dict()
+
+    print("combien de recettes voulez vous ajouter?")
+    nb_entries = int(input())
+
+    for i in range(nb_entries):
+        print(f"Quel est le nom de votre recette numéro {i}: ")
+        recette = input()
+
+        print("Quels sont les ingrédients de cette recette "
+              "(laissez un espace entre chaque ingrédient): ")
+        ingredients = input().split(" ")
+
+        livre_recette[recette] = ingredients
+
+    return livre_recette
+
+
+def print_recipe(ingredients) -> None:
+    # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
+    recette = ""
+
+    while recette not in ingredients:
+
+        print("Quel est le nom de la recette que vous cherchez: ")
+        recette = input()
+
+        if recette in ingredients:
+            print("Voici les ingrédients de la recette choisi:", ingredients[recette])
+        else:
+            print("Désolé, mais cette recette n'est pas dans le livre de recette")
+
+
+def main() -> None:
+    print(f"On essaie d'ordonner les valeurs...")
+    order()
+
+    print(f"On vérifie les anagrammes...")
+    anagrams()
+
+    my_list = [3, 3, 5, 6, 1, 1]
+    print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
+
+    grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
+    best_student = best_grades(grades)
+    print(f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}")
+
+    sentence = "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
+    print(frequence(sentence))
+
+    print("On enregistre les recettes...")
+    recipes = get_recipes()
+
+    print("On affiche une recette au choix...")
+    print_recipe(recipes)
+
+
+if __name__ == '__main__':
+    main()
